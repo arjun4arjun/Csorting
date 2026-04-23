374b7945060e47986e47b3c75a76ddcee5cafc9b
redwood <arjunmehrania@gmail.com>
2026-04-23 19:31:33 +0530

    learnt operations on doubly linked lists, deletion at beginning, insertion at beginning and end, printing and reversing the ll

--- LL/doubly_ll.c
+++ LL/doubly_ll.c
@@ -35,7 +35,7 @@ if (head==NULL){
     return head;
 }
 
-int print_s(struct Node*head){
+int print_f(struct Node*head){
 struct Node*temp=head;
 while (temp != NULL){
     printf("%d, ", temp->data);
@@ -43,11 +43,36 @@ while (temp != NULL){
 }
 }
 
+void print_e(struct Node*head){
+    struct Node*temp=head;
+    while(temp->next !=NULL){
+        temp=temp->next;
+    }
+    while(temp !=NULL){
+        printf("%d",temp->data);
+        temp=temp->prev;
+    }
+
+}
+struct Node*delete_f(struct Node*head){
+    if (head==NULL){
+        return NULL;
+    }
+    if(head->next !=NULL){
+        head=head->next;
+        head->prev=NULL;
+    }
+
+    return head;
+}
 int main(){
     struct Node*head=NULL;
-    head=insertion_e(head,10);
+    head = insertion_e(head,10);
     head = insertion_e(head,30);
     head = insertion_e(head,90);
-    print_s(head);
+    head= insertion_f(head, 05);
+    head=delete_f(head);
+    print_f(head);
+    print_e(head);
     return 0;
 }
